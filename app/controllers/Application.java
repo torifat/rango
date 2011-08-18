311d044378c6612a04a7e6398ad2548bca10e0f8
Mehdi Hasan <mhasan@omicronlab.com>
2011-08-18 12:13:01 +0600

    Comment out test codes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,18 +11,18 @@ import models.*;
 public class Application extends Controller {
     
     public static void index() {
-    
-        Category root = new Category("Root").save();
-        Logger.debug("Category %s added with id %d", root.name, root.id);
-        new Category("Sub1", root.id).save();
-        new Category("Sub2", root.id).save();
-        new Category("Sub3", root.id).save();
         
-        List<Category> ls = root.getChildren();
-        
-        for (Category c : ls) {
-            Logger.debug("Category Name: %s", c.name);
-        }
+        /*        Category root = new Category("Root").save();
+                Logger.debug("Category %s added with id %d", root.name, root.id);
+                new Category("Sub1", root.id).save();
+                new Category("Sub2", root.id).save();
+                new Category("Sub3", root.id).save();
+                
+                List<Category> ls = root.getChildren();
+                
+                for (Category c : ls) {
+                    Logger.debug("Category Name: %s", c.name);
+                }*/
         
         render();
     }
